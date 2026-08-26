9d780f76cb371f929ac52eb8afa61e4cffae3edc
Kathirvel LK <kathirvellkbe@gmail.com>
2026-08-26 12:45:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// 1. Create variables for student name, age and mark
+
+let studentName = "Kathir";
+let studentAge = 24;
+let mark = 90;
+
+console.log("Student Name: " + studentName);
+console.log("Age: " + studentAge);
+console.log("Mark: " + mark);
+
+
+
+// 2. Ask name using prompt() and display using alert()
+
+let name = prompt("Enter your name:");
+
+alert("Kathir " + name);
+
+
+
+// 3. Ask age using prompt() and print using console.log()
+
+let userAge = prompt("Enter your age:");
+
+console.log("Your age is : " + userAge);
+
+
+
+
+// 4. Ask question using confirm()
+
+let answer = confirm("Do you like JavaScript?");
+
+console.log("Your answer: " + answer);
+
+
+
+// 5. Ask name and display using document.writeln()
+
+let webpageName = prompt("Enter your name:");
+
